04a0589d748ebeeb963650ebdb46a9a9d2482465
CalebeSG <calebeguimaraes10@gmail.com>
2020-10-06 19:59:42 -0300

    redux flow implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
+import { store } from './store';
 
 import SignIn from './pages/SignIn';
 
-const App = () => <SignIn />;
+function App() {
+  return (
+    <Provider store={store}>
+      <SignIn />
+    </Provider>
+  );
+}
 
 export default App;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-import logo from '../../assets/logo.png';
+import { StoreState } from '../../store/createStore';
+import { signInRequest } from '../../store/modules/auth/actions';
 
 import './styles.css';
+import logo from '../../assets/logo.png';
 
 function SignIn() {
+  const { loadingSignInRequest } = useSelector((state: StoreState) => state.auth);
+  const dispatch = useDispatch();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  console.log('LOADING: ', loadingSignInRequest);
+
   return (
     <div className="sign-in-page">
       <img src={logo} alt="logo sign in"/>
       
       <input 
-        type="text" 
+        type="text"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
         placeholder="Email"
-        defaultValue="email@email.com"
       />
       <input 
         type="password" 
+        value={password}
+        onChange={e => setPassword(e.target.value)}
         placeholder="Password"
-        defaultValue="123456"
       />
 
-      <button onClick={() => {}}>Entrar</button>
+      <button 
+        onClick={() => dispatch(signInRequest({ email, password }))}
+      >
+        { loadingSignInRequest ? 'Carregando' : 'Entrar' }
+      </button>
     </div>
   );
 }
